cd611bfa248b0119f71c0b17856fc2774d6e6566
Neithari <29165243+Neithari@users.noreply.github.com>
2017-07-23 10:33:11 +0200

    Fixed alignment

--- Engine/Alien.cpp
+++ Engine/Alien.cpp
@@ -6,28 +6,35 @@ Alien::Alien( Graphics & gfx )
 {
 	//small
 	Dimention i_dim = invaderSmall[0].GetDim();
+	Dimention iBig_dim = invaderBig[0].GetDim();
 	Location i_loc = loc;
+	float align= iBig_dim.width / 2 - i_dim.width / 2;
+	i_loc.x += align;
 	invaderSmall[0].Init( i_loc );
 	for ( int i = 1; i < n_small; i++ )
 	{
-		i_loc.x += i_dim.width + alienSpacing;
+		i_loc.x += iBig_dim.width + alienSpacing;
 		invaderSmall[i].Init( i_loc );
 	}
 	//mid
 	i_dim = invaderMid[0].GetDim();
+	align = iBig_dim.width / 2 - i_dim.width / 2;
 	i_loc.x = loc.x;
+	i_loc.x += align;
 	i_loc.y += i_dim.height + alienSpacing;
 	invaderMid[0].Init( i_loc );
 	for ( int i = 1; i < n_mid/2; i++ )
 	{
-		i_loc.x += i_dim.width + alienSpacing;
+		i_loc.x += iBig_dim.width + alienSpacing;
 		invaderMid[i].Init( i_loc );
 	}
 	i_loc.x = loc.x;
+	i_loc.x += align;
 	i_loc.y += i_dim.height + alienSpacing;
-	for ( int i = n_mid / 2; i < n_mid; i++ )
+	invaderMid[n_mid / 2].Init( i_loc );
+	for ( int i = 1 + n_mid / 2; i < n_mid; i++ )
 	{
-		i_loc.x += i_dim.width + alienSpacing;
+		i_loc.x += iBig_dim.width + alienSpacing;
 		invaderMid[i].Init( i_loc );
 	}
 	//big
@@ -42,7 +49,8 @@ Alien::Alien( Graphics & gfx )
 	}
 	i_loc.x = loc.x;
 	i_loc.y += i_dim.height + alienSpacing;
-	for ( int i = n_big / 2; i < n_big; i++ )
+	invaderBig[n_big / 2].Init( i_loc );
+	for ( int i = 1 + n_big / 2; i < n_big; i++ )
 	{
 		i_loc.x += i_dim.width + alienSpacing;
 		invaderBig[i].Init( i_loc );
