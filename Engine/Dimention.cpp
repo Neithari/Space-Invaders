25f0bbea9b09d99bcac9d87361e6c516b7a0aa7d
Neithari <29165243+Neithari@users.noreply.github.com>
2017-08-29 20:05:22 +0200

    upgraded dimention with a bunch of Operators simmilar to Location

--- Engine/Dimention.cpp
+++ Engine/Dimention.cpp
@@ -4,3 +4,43 @@ bool Dimention::operator==( const Dimention & rhs ) const
 {
 	return width == rhs.width && height == rhs.height;
 }
+
+Dimention Dimention::operator+( const Dimention& rhs ) const
+{
+	return Dimention( width + rhs.width,height + rhs.height );
+}
+
+Dimention& Dimention::operator+=( const Dimention& rhs )
+{
+	return *this = *this + rhs;
+}
+
+Dimention Dimention::operator*( int rhs ) const
+{
+	return Dimention( width * rhs,height * rhs );
+}
+
+Dimention& Dimention::operator*=( int rhs )
+{
+	return *this = *this * rhs;
+}
+
+Dimention Dimention::operator-( const Dimention& rhs ) const
+{
+	return Dimention( width - rhs.width,height - rhs.height );
+}
+
+Dimention& Dimention::operator-=( const Dimention& rhs )
+{
+	return *this = *this - rhs;
+}
+
+Dimention Dimention::operator/( int rhs ) const
+{
+	return Dimention( width / rhs,height / rhs );
+}
+
+Dimention& Dimention::operator/=( int rhs )
+{
+	return *this = *this / rhs;
+}
--- Engine/Dimention.h
+++ Engine/Dimention.h
@@ -4,8 +4,22 @@ class Dimention
 {
 public:
 	Dimention() = default;
-	
+	constexpr Dimention( int width_in,int height_in )
+		:
+		width( width_in ),
+		height( height_in )
+	{
+	};
+
 	bool operator==( const Dimention& rhs ) const;
+	Dimention operator+( const Dimention& rhs ) const;
+	Dimention& operator+=( const Dimention& rhs );
+	Dimention operator*( int rhs ) const;
+	Dimention& operator*=( int rhs );
+	Dimention operator-( const Dimention& rhs ) const;
+	Dimention& operator-=( const Dimention& rhs );
+	Dimention operator/( int rhs ) const;
+	Dimention& operator/=( int rhs );
 public:
 	int width;
 	int height;
